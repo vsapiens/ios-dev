54ae936303331b1a13d1dcf2f070fa5cb57b3216
vsapiens <erickfrank@icloud.com>
2020-02-15 11:46:34 -0600

    Added divisas

--- Calculadora/Calculadora/ViewController.swift
+++ Calculadora/Calculadora/ViewController.swift
@@ -16,13 +16,25 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var tfParcial: UITextField!
-    
     @IBOutlet weak var tfActividades: UITextField!
     @IBOutlet weak var tfExamen: UITextField!
     @IBOutlet weak var lbExamen: UILabel!
     @IBOutlet weak var tfFinal: UITextField!
     @IBOutlet weak var lbFinal: UILabel!
     
-    @IBAction func calculaNota(_ sender: Any) {    }
+    @IBAction func calculaNota(_ sender: Any) {
+        if let parcial = Double(tfParcial.text!),
+            let actividades = Double(tfActividades.text!),
+            let final = Double(tfExamen.text!){
+            let calificacion = tfParcial*0.5 +tfActividades*0.2 + tfExamen*0.3
+            tfFinal.text = "\(calificacion)"
+        }
+        else {
+            let alert = UIAlertController(title: "Error", message: "Los campos deben de tener valor númerico.", preferredStyle: .alert)
+            let accion = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(accion)
+            present(alert,animated: true,completion: nil)
+        }
+    }
 }
 
