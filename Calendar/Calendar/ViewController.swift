41ca0c63e9eedc6fc95313338c3ba678bda7a98f
vsapiens <erickfrank@icloud.com>
2020-02-26 15:00:47 -0600

    Finished calendar

--- Calendar/Calendar/ViewController.swift
+++ Calendar/Calendar/ViewController.swift
@@ -9,22 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let formatter: DateFormatter = DateFormatter()
+    var now = Date()
+    var dateComponents = DateComponents()
+    
     @IBOutlet weak var lbYear: UILabel!
     @IBOutlet weak var lbMonth: UILabel!
     @IBOutlet weak var lbDay: UILabel!
     @IBOutlet weak var lbDayNum: UILabel!
-    let formatter = DateFormatter()
-    formatter.dateformat = ""
-    @IBAction func swipeNext(_ sender: Any) {
+
+    func update(){
+        formatter.dateFormat = "MMMM"
+        lbMonth.text = formatter.string(from: now)
+        formatter.dateFormat = "dd"
+        lbDayNum.text = formatter.string(from: now)
+        formatter.dateFormat = "EEEE"
+        lbDay.text = formatter.string(from: now)
+        formatter.dateFormat = "yyyy"
+        lbYear.text = formatter.string(from: now)
     }
+    
     @IBAction func swipeLast(_ sender: Any) {
+        
+        dateComponents.setValue(-1, for: .day)
+        now = Calendar.current.date(byAdding: dateComponents, to: now)!
+        update()
+    }
+    @IBAction func swipeNext(_ sender: Any) {
+        dateComponents.setValue(1, for: .day)
+        now = Calendar.current.date(byAdding: dateComponents, to: now)!
+        update()
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        lbYear.text = formatter.
+        update()
+        lbDayNum.adjustsFontSizeToFitWidth = true
+        lbDayNum.minimumScaleFactor = 0.2
     }
+    
 
 }
 
